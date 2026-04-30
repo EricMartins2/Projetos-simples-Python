7613f9882292f08292669bb03685ab4ef7cebaa8
Éric Martins <ericgaidargimartins@gmail.com>
2026-04-30 18:46:20 -0300

    Coloquei para mostrar o processo de cálculo

--- CálculoFatorial.py
+++ CálculoFatorial.py
@@ -1,9 +1,13 @@
 num = int(input('Escolha um número: '))
 num2 = num - 1
 mult = num * num2
+print('', num, end='')
 while num2 != 1:
+    print(' x {}'.format(num2), end='')
     num2 -= 1
     mult = mult * num2
+    if num2 == 1:
+        print(' x {} = {}'.format(num2, mult))
 
 
 print('O fatorial de {}! é igual a {}'.format(num, mult))
