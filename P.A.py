6c8a61e7a877fd8f465533e9c502fb8119f6808a
Éric Martins <ericgaidargimartins@gmail.com>
2026-05-05 17:33:24 -0300

    Cálculo P.A.
    O programa te dá a opção de ver quantos termos você quiser.

--- P.A.py
+++ P.A.py
@@ -1,9 +1,15 @@
 n1 = int(input('Primeiro termo: '))
 razao = int(input('Qual a razão: '))
-n10 = n1 + (razao * 10)
+cont = 1
 posicao = 0
-while n1 != n10:
-    posicao += 1
-    print('{}° Termo -> {}'.format(posicao, n1))
-    n1 += razao
-print('Esses são os 10 primeiros termos!')
+repeticoes = 10
+escolha = 1
+while escolha != 0:
+    while cont <= repeticoes:
+        posicao += 1
+        print('{}° Termo -> {}'.format(posicao, n1))
+        n1 += razao
+        cont += 1
+    escolha = int(input('Quantos termos a mais você quer? '))
+    repeticoes += escolha
+print('Esses são os {} termos da P.A!'.format(repeticoes))
